62e645f9ecadcec0bea63ee866756bab30a3aa24
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2022-06-02 10:21:51 +0200

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -252,6 +252,8 @@ nav ul li a:hover {
    <!-- ll -->
    <!-- ll -->
    <!-- ll -->
+      <!-- ll -->
+
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
