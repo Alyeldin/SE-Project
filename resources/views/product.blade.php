1c7751b1f50944944db46909f0f9908f7097bf29
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2022-06-03 14:38:33 +0200

    admin changes

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -146,9 +146,7 @@
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                  
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
+              
             </div>
           </div>
         </div>
@@ -161,9 +159,7 @@
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
+                
             </div>
           </div>
         </div>
@@ -176,9 +172,7 @@
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                 
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
+                
             </div>
           </div>
         </div>
@@ -192,9 +186,7 @@
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
+               
             </div>
           </div>
         </div>
@@ -206,10 +198,7 @@
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
+               
             </div>
           </div>
         </div>
@@ -222,9 +211,7 @@
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                  
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
+                
             </div>
           </div>
         </div>
@@ -238,24 +225,7 @@
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
+                
             </div>
           </div>
         </div>
@@ -268,9 +238,19 @@
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                 
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                
+               
             </div>
           </div>
         </div>
